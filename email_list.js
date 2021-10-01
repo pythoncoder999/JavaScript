b2667d6e2a65e3c431b277ab06f795efda1b7222
Pythoncoder999 <pythoncoder999@gmail.com>
2021-10-01 00:31:15 -0500

    Adding more code and js file

---
+++ email_list.js
@@ -0,0 +1,10 @@
+var $ = function (id) {
+	
+	return document.getElementById(id);
+	
+}
+
+var joinList = function () {
+	var emailAddress1 = $("emailaddress1").value;
+	var emailAddress2 = $("emailaddress2").value;
+}
