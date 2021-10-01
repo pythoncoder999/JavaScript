8d273172fe309609db59b06cebd2e3d3ab964d22
Pythoncoder999 <pythoncoder999@gmail.com>
2021-10-01 00:38:59 -0500

    Adding more code js

--- email_list.js
+++ email_list.js
@@ -7,4 +7,15 @@ var $ = function (id) {
 var joinList = function () {
 	var emailAddress1 = $("emailaddress1").value;
 	var emailAddress2 = $("emailaddress2").value;
-}
+	var isValid = true;
+
+	if (emailAddress1 == "") {
+		$("email_address1_error").firstChild.nodeValue = "This field is required.";
+		isValid = false;
+	} else {$("email_address1_error").fistChild.nodeValue = "";}
+	
+	if (emailAddress1 !== emailAddress2) {
+		$("email_address2_error").firstChild.nodeValue = "This entry must equal first entry."; isValid = false;
+		
+	} else {$(email_address2_error).firstChild.nodeValue = "";}
+	}
