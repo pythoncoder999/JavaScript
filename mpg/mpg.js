d6b491135bade92fee723d44d2f452a7b7ada81a
Gunardi <pythoncoder999@gmail.com>
2021-10-02 18:55:17 -0500

    adding some script

--- mpg/mpg.js
+++ mpg/mpg.js
@@ -1,12 +1,39 @@
 // alert("MPG Calculator")
 
-alert("The Calculate MPG application")
-var miles = prompt("Enter miles driven");
-miles = parseFloat(miles);
+// alert("The Calculate MPG application")
+// var miles = prompt("Enter miles driven");
+// miles = parseFloat(miles);
 
-var gallons = prompt("Enter gallons of gas used");
-gallons = parseFloat(gallons);
+// var gallons = prompt("Enter gallons of gas used");
+// gallons = parseFloat(gallons);
 
-var mpg = miles/gallons;
-mpg = parseInt(mpg);
-alert("Miles per gallon = " + mpg);
+// var mpg = miles/gallons;
+// mpg = parseInt(mpg);
+// alert("Miles per gallon = " + mpg);
+
+"use strict"
+var $ = function(id) {
+	return document.getElementById(id);
+}
+
+var calculateMPG = function(miles, gallons) {
+	var mpg = (miles/gallons);
+	mpg = mpg.toFixed(1);
+	return mpg;
+}
+
+var processEntries = function () {
+	var miles = parseFloat($("miles").value);
+	var gallons = parseInt($("gallons").value);
+	
+	if (isNaN(miles) || isNaN(gallons)) {
+		alert("Both entries must be numeric");
+	}
+	else {
+		$("mpg").value = calculateMPG(miles, gallons);
+	}
+}
+window.onload = function() {
+	$("calculate").onclick = processEntries;
+	$("miles").focus();
+}
